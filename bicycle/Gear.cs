e7514d05637ebd34027ccb13f69ecf5d35466fe9
mmatthew <takeknock@gmail.com>
2017-10-09 14:28:38 +0900

    add ratio property

---
+++ bicycle/Gear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bicycle
+{
+    public class Gear
+    {
+        public Gear(double chainring, double cog)
+        {
+            _chainring = chainring;
+            _cog = cog;
+        }
+
+        public double ratio
+        {
+            get{ return _chainring / _cog;}
+        }
+        private double _chainring;
+        private double _cog;
+
+    }
+}
