a95286d463ab5f84b8db16637deef30aa9e230b5
Takehiro M <takeknock@gmail.com>
2017-10-09 14:32:18 +0900

    add ratio property (#1)

---
+++ poodr/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using bicycle;
+
+namespace poodr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double chainring = 52;
+            double cog = 11;
+            Gear gear = new Gear(chainring, cog);
+            Console.WriteLine(gear.ratio.ToString());
+        }
+    }
+}
