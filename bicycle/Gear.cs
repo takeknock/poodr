abe8023b082fcc51c9a03337c0bb0a411c451ebd
Takehiro M <takeknock@gmail.com>
2017-10-09 15:04:01 +0900

    add gearInches property (#3)

--- bicycle/Gear.cs
+++ bicycle/Gear.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace bicycle
+namespace Bicycle
 {
     public class Gear
     {
@@ -14,12 +14,28 @@ namespace bicycle
             _cog = cog;
         }
 
+
+        public Gear(double chainring, double cog, double rim, double tire)
+        {
+            _chainring = chainring;
+            _cog = cog;
+            _rim = rim;
+            _tire = tire;
+        }
+
         public double ratio
         {
             get{ return _chainring / _cog;}
         }
+
+        public double gearInches
+        {
+            get { return ratio * (_rim + (_tire * 2)); }
+        }
         private double _chainring;
         private double _cog;
+        private double _rim;
+        private double _tire;
 
     }
 }
