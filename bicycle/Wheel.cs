07294066bb7b57bf6b0fd6d5267958a629808a2a
mmatthew <takeknock@gmail.com>
2017-10-11 21:58:42 +0900

    add Wheel class

---
+++ bicycle/Wheel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bicycle
+{
+    public class Wheel
+    {
+        Wheel(double rim, double tire)
+        {
+            _rim = rim;
+            _tire = tire;
+        }
+
+        private double _rim;
+        private double _tire;
+
+        public double rim
+        {
+            get { return _rim;}
+        }
+
+        public double tire
+        {
+            get{ return _tire;}
+        }
+    }
+}
