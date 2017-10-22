0744ea6add58f3a1a78c13559594ff69462473bc
mmatthew <takeknock@gmail.com>
2017-10-22 21:00:21 +0900

    2.3.2: add ObscuringReferences class

---
+++ bicycle/ObscuringReferences.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bicycle
+{
+    class ObscuringReferences
+    {
+        ObscuringReferences(List<Tuple<double,double>> data)
+        {
+            _data = data;
+        }
+
+        public IEnumerable<double> diameter()
+        {
+            return data.Select(e => e.Item1 + (e.Item2 * 2.0));
+        }
+
+        private List<Tuple<double, double>> data
+        {
+            get { return _data; }
+        }
+
+        private List<Tuple<double, double>> _data;
+    }
+}
